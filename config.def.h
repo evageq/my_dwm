7fe30912217d8cfc3b97ed687bbc7e3e912873c3
evageq <gostevevg0@gmail.com>
2026-01-12 18:02:05 +0300

    config.def.h: Add via cmd

--- config.def.h
+++ config.def.h
@@ -60,6 +60,7 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "bash", "-c", cmd, NULL } }
 
 /* commands */
+static char *viacmd[] = {"via", NULL};
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
@@ -75,7 +76,8 @@ static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-    { MODKEY,                       XK_v,      spawn,          {.v = clipboardcmd } },
+    { MODKEY|ShiftMask,             XK_v,      spawn,          {.v = clipboardcmd } },
+    { MODKEY,                       XK_v,      spawn,          {.v = viacmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
